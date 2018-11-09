607db970dc6951ee7c2711211259701d84f7f428
3TDesigner <tanto.adebowale@gmail.com>
2018-11-09 15:54:04 -0600

    Go Fish - Program 6

--- card.cpp
+++ card.cpp
@@ -13,34 +13,7 @@ Card::Card(int rank, Suit s){
 }
 
 string Card::toString() const {
-    string myCard = "";
-    const int CONVERT_INT = 48;
-
-    if(myRank == 1){
-        myCard += "A";
-    }else if(myRank >= 2 && myRank <= 9){
-        myCard += myRank + CONVERT_INT;
-    }else if(myRank == 10){
-        myCard += "10";
-    }else if(myRank == 11){
-        myCard += "J";
-    }else if(myRank == 12){
-        myCard += "Q";
-    }else if(myRank == 13){
-        myCard += "K";
-    }
-
-    if(mySuit == spades){
-        myCard += "s";
-    }else if(mySuit == hearts){
-        myCard += "h";
-    }else if(mySuit == diamonds){
-        myCard += "d";
-    }else if(mySuit == clubs){
-        myCard += "c";
-    }
-
-    return myCard;
+    return rankString(myRank) + suitString(mySuit);
 }
 
 bool Card::sameSuitAs(const Card &c) const {
@@ -71,24 +44,9 @@ string Card::suitString(Card::Suit s) const {
 }
 
 string Card::rankString(int r) const {
-    string myCard = "";
-    const int CONVERT_INT = 48;
+    string Ranks[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-    if(r == 1){
-        myCard += "A";
-    }else if(r >= 2 && r <= 10){
-        myCard += myRank + CONVERT_INT;
-    }else if(r == 10){
-        myCard += "10";
-    }else if(r == 11){
-        myCard += "J";
-    }else if(r == 12){
-        myCard += "Q";
-    }else if(r == 13){
-        myCard += "K";
-    }
-
-    return myCard;
+    return Ranks[r-1];
 }
 
 bool Card::operator==(const Card &rhs) const {
